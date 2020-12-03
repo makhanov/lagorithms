17ed292c90e47ff96d12549724f4b883c9c1de63
Nursultan Makhanov <makhanov91@gmail.com>
2020-12-03 12:38:36 +0600

    132pattern.py is created

---
+++ leetcode/132Pattern.py
@@ -0,0 +1,21 @@
+'''
+Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
+
+Return true if there is a 132 pattern in nums, otherwise, return false.
+
+Follow up: The O(n^2) is trivial, could you come up with the O(n logn) or the O(n) solution?
+
+Input: nums = [1,2,3,4]
+Output: false
+Explanation: There is no 132 pattern in the sequence.
+Hint: Stack
+'''
+def find132pattern(nums):
+    n = len(nums)
+    stack = []
+    for i in nums:
+        if something:
+            return True
+
+        else:
+            return False
